8216fdcce99d43aad574f3891482cac7f8a744e2
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-03-19 12:18:44 -0300

    aula14 | estrutura for loop + painel de depuração

---
+++ exercicios-aula/aula14/script-1.js
@@ -0,0 +1,15 @@
+// Testando o depurador
+
+// F5 : iniciar depuração;
+//      (dentro do depurador o F5 pula comandos inteiros);
+// F10 : passo a passo de cada comando;
+// Shift + F5 : sair da depuração; e
+// O resto é pra depois.
+
+console.log('Vai começar ...')
+
+for (let i=0;i<=5;i++) {
+    console.log(i)
+}
+
+console.log('FIM!')
---
+++ exercicios-aula/aula14/script.js
@@ -0,0 +1,6 @@
+// Estrutura de repetição `for (inicio; teste; incremento) {...}`
+
+for (let i = 1; i <= 1_000; i++) {
+    console.log(`passo ${i}`)
+    console.log('Texto')
+}
