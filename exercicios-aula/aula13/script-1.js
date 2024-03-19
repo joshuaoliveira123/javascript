76e58bc9b418e479becb4485965eee1edbc490da
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-03-19 11:31:49 -0300

    aula13 | estruturas de repetição while e do-while

---
+++ exercicios-aula/aula13/script-1.js
@@ -0,0 +1,8 @@
+// Estrutura de repetição `do {...} while (...)`
+
+var c = 1
+do {
+    console.log(`contador = ${c}`)
+    console.log('Número menor ou igual a 10')
+    c++
+} while (c <= 10)
---
+++ exercicios-aula/aula13/script.js
@@ -0,0 +1,16 @@
+// Sem usar estrutura de repetição:
+
+// console.log('Olá, tudo bem?')
+// console.log('Olá, tudo bem?')
+// console.log('Olá, tudo bem?')
+// console.log('Olá, tudo bem?')
+// console.log('Olá, tudo bem?')
+// console.log('Olá, tudo bem?')
+
+// Estrutura de repetição `while (...) {...}`
+var c = 1
+while (c <= 10) {
+    console.log('Olá, tudo bem?')
+    console.log(`Passo ${c}`)
+    c++
+}
