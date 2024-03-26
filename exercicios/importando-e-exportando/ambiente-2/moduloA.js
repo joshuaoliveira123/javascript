7ed29cd518ffd22a9c4a776df0a132693384ad6f
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-03-26 15:56:51 -0300

    anotação

--- exercicios/importando-e-exportando/ambiente-2/moduloA.js
+++ exercicios/importando-e-exportando/ambiente-2/moduloA.js
@@ -1,4 +1,8 @@
 const variavel = 'Valor'
 const variavel2 = 'Outro valor'
+const getFullName = (firstName, lastName) => {
+    return `Meu nome completo é ${firstName} ${lastName}!`
+}
 
-module.exports = { variavel }
+module.exports = { variavel, getFullName }
+// module.exports = { getFullName } | Apenas um module.exports por arquivo é o ideal, ou da uns bug
--- exercicios/importando-e-exportando/ambiente-2/moduloC.js
+++ exercicios/importando-e-exportando/ambiente-2/moduloC.js
@@ -1,3 +1,4 @@
 const v = require('./moduloA')
 
 console.log(v.variavel)
+console.log(v.getFullName('Joshua', 'Oliveira'))
