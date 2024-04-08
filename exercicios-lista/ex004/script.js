da52be02b56b7371cce21329a2663d16128c574e
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-04-08 10:10:23 -0300

    exercicios-lista | 4

---
+++ exercicios-lista/ex004/script.js
@@ -0,0 +1,11 @@
+// 4) Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório 
+// entre eles. 
+// Ex: 
+// Digite um valor: 8 
+// Digite outro valor: 5 
+// A soma entre 8 e 5 é igual a 13.
+const soma = (a, b) => a + b
+const n1 = 5
+const n2 = 8
+
+console.log(`${n1} + ${n2} = ${soma(n1, n2)}`)
