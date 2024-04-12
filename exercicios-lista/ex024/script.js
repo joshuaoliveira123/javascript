187fda23be09e746046053305184eedcb7b86ef9
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-04-12 10:33:12 -0300

    exercicios-lista | 24

---
+++ exercicios-lista/ex024/script.js
@@ -0,0 +1,23 @@
+// 24) Faça um algoritmo que pergunte a distância que um passageiro deseja 
+// percorrer em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para 
+// viagens até 200Km e R$0.45 para viagens mais longas.
+
+const passagem = function(distancia) {
+    let valorPorKm = 0.5
+
+    if (distancia > 200) {
+        valorPorKm = 0.45
+    }
+
+    return distancia * valorPorKm
+}
+
+const moedaBRL = { style: 'currency', currency: 'BRL' }
+
+const viagem1 = passagem(77).toLocaleString('pt-br', moedaBRL)
+const viagem2 = passagem(260).toLocaleString('pt-br', moedaBRL)
+const viagem3 = passagem(197).toLocaleString('pt-br', moedaBRL)
+
+console.log(`Total a pagar na 1ª viagem: ${viagem1}`)
+console.log(`Total a pagar na 2ª viagem: ${viagem2}`)
+console.log(`Total a pagar na 3ª viagem: ${viagem3}`)
