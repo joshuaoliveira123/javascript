2b9b9d02ebbd4e454d30b12f13386383e25bd8bb
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-04-19 15:01:02 -0300

    exercicios-lista | update 48

--- exercicios-lista/ex048/soma-7-numeros.js
+++ exercicios-lista/ex048/soma-7-numeros.js
@@ -1,7 +1,7 @@
 // 48) Faça um programa que leia 7 números inteiros e no final mostre o somatório 
 // entre eles.
 
-import { randomNum } from "../../exercicios/numero-aleatorio.js"
+import { randomNum } from '../../exercicios/numero-aleatorio.js'
 
 const nums = new Array(7)
 let sum = 0
