e671da8b6ffa23c4cda74546f8f96dbccc0ca7a4
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-04-20 13:51:57 -0300

    exercicios-lista | 53

---
+++ exercicios-lista/ex053/pessoas.js
@@ -0,0 +1,63 @@
+// 53) Faça um programa que leia a idade e o sexo de 5 pessoas, mostrando no final: 
+//  a) Quantos homens foram cadastrados 
+//  b) Quantas mulheres foram cadastradas 
+//  c) A média de idade do grupo 
+//  d) A média de idade dos homens 
+//  e) Quantas mulheres tem mais de 20 anos
+
+import { media } from '../../exercicios/media.js'
+import { randomNum, randomMF } from '../../exercicios/numero-aleatorio.js'
+
+const pessoas = {
+    pessoa1: {
+        idade: randomNum(100),
+        sexo: randomMF()
+    },
+    pessoa2: {
+        idade: randomNum(100),
+        sexo: randomMF()
+    },
+    pessoa3: {
+        idade: randomNum(100),
+        sexo: randomMF()
+    },
+    pessoa4: {
+        idade: randomNum(100),
+        sexo: randomMF()
+    },
+    pessoa5: {
+        idade: randomNum(100),
+        sexo: randomMF()
+    }
+}
+
+const infoPessoas = function(pessoas) {
+    const idades = []
+    const idadeHomens = []
+    let homens = 0
+    let mulheres = 0
+    let mulheres20 = 0
+
+    for (const pessoa in pessoas) {
+        let i = 1
+        idades.push(pessoas[pessoa].idade)
+
+        if (pessoas[pessoa].sexo.toUpperCase() === 'M') {
+            homens++
+            idadeHomens.push(pessoas[pessoa].idade)
+        }
+
+        if (pessoas[pessoa].sexo.toUpperCase() === 'F') {
+            mulheres++
+            mulheres20 += pessoas[pessoa].idade > 20 ? 1 : 0
+        }
+    }
+
+    console.log(`Número Homens: ${homens}`)
+    console.log(`Número Mulheres: ${mulheres}`)
+    console.log(`Média idade grupo: ${media(idades).toFixed(1)}`)
+    console.log(`Média idade homens: ${media(idadeHomens).toFixed(1)}`)
+    console.log(`Número de mulheres com mais de 20 anos: ${mulheres20}`)
+}
+
+infoPessoas(pessoas)
