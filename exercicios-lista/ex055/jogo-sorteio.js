82ecacb402c658469b5cf421be8177d824430fcc
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-04-23 16:45:40 -0300

    exercicios-lista | 55
    F I N A L M E N T E ... eu desisti do readline!
    Tenho que estudar mais pra conseguir fazer aquilo.

--- exercicios-lista/ex055/jogo-sorteio.js
+++ exercicios-lista/ex055/jogo-sorteio.js
@@ -2,23 +2,19 @@
 // agora, o computador vai sortear um número entre 1 e 10 e o jogador vai ter 4 
 // tentativas para tentar acertar.
 
-const readline = require('node:readline')
-const { stdin: input, stdout: output } = require('node:process')
-
-const rl = readline.createInterface({ input, output })
-
 function jogo(vidas = 4) {
     const numAleatorio = Math.floor(Math.random() * ((10 + 1) - 1) + 1)
+    const nums = []
+    alert('Vamos Jogar! É simples, apenas escolha um número entre 1 e 10. Se acertar você vence, se errar 4 vezes, perde!\n\nobs: caso envie qualquer coisa que não seja a resposta, terá vidas descontadas!')
     while (vidas > 0) {
-        // limpar tela
-        console.log(`VIDAS: ${vidas}`)
-        // chute | rl.question(pergunta, callback)
-            // callback:
-                // se acertar SAIR DO LOOP
-                // se errar vidas--
+        let res = prompt(`VIDAS: ${vidas} \nDigite um número (1 a 10)!`)
+        nums.push(Number(res))
+        if (Number(res) === numAleatorio) {
+            break
+        } else {
+            vidas--
+        }
     }
     const resultado = (vidas > 0) ? 'VENCEU' : 'PERDEU'
-    console.log(resultado)
+    alert(`Número sorteado: ${numAleatorio}\nNúmeros escolhidos: ${nums}\nVocê ${resultado}!`)
 }
-
-jogo()
