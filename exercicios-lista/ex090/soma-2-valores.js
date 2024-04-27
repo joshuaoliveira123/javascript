7a6c6f71f220109b8f6277cd29763c1db0466f65
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-04-27 20:45:10 -0300

    exercicios-lista | 90

---
+++ exercicios-lista/ex090/soma-2-valores.js
@@ -0,0 +1,9 @@
+// 90) Desenvolva um algoritmo que leia dois valores pelo teclado e passe esses 
+// valores para um procedimento Somador() que vai calcular e mostrar a soma entre 
+// eles.
+
+const soma = (a, b) => console.log(a + b)
+
+soma(5,2)
+soma(7,3)
+soma(19, -5)
