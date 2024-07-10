6b41382af17647689504b73778d0eef638897987
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-07-10 19:25:22 -0300

    hoisting
    Testando algumas demonstrações de hoisting no javascript.

---
+++ exercicios/hoisting/cat-name.js
@@ -0,0 +1,10 @@
+function catName(name) {
+    console.log(`O nome do gato é ${name}.`);
+}
+
+catName('Rookie');
+dogName('Toby');
+
+function dogName(name) {
+    console.log(`O nome do Cachorro é ${name}.`);
+}
