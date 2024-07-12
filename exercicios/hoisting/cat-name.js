58a4478655f695bd610b60a3f06314f8b3c47c85
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-07-11 21:05:49 -0300

    padronizando
    removendo "pontos e virgulas" para ficar igual ao padrão que estava sendo feito antes.

--- exercicios/hoisting/cat-name.js
+++ exercicios/hoisting/cat-name.js
@@ -1,10 +1,10 @@
 function catName(name) {
-    console.log(`O nome do gato é ${name}.`);
+    console.log(`O nome do gato é ${name}.`)
 }
 
-catName('Rookie');
-dogName('Toby');
+catName('Rookie')
+dogName('Toby')
 
 function dogName(name) {
-    console.log(`O nome do Cachorro é ${name}.`);
+    console.log(`O nome do Cachorro é ${name}.`)
 }
--- exercicios/hoisting/exemple.js
+++ exercicios/hoisting/exemple.js
@@ -1,20 +1,20 @@
-foo();
-bar();
-baz();
+foo()
+bar()
+baz()
 
 function foo() {
-    console.log(a);
-    var a = 2;
+    console.log(a)
+    var a = 2
 }
 
 function bar() {
-    let a = 3;
-    console.log(a);
+    let a = 3
+    console.log(a)
 }
 
 // Na documentação do MDN, isso deveria funcionar, mas da erro, não sei do porquê. Mas acho que isso é bom.
 function baz() {
-    // num = 5;
-    // console.log(num);
-    // let num;
+    // num = 5
+    // console.log(num)
+    // let num
 }
