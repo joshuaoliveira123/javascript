247c21e7292637269851711ed1078528ab337a44
Joshua Oliveira <125186376+joshuaoliveira123@users.noreply.github.com>
2024-07-23 11:11:02 -0300

    Update try-catch.js

--- exercicios/try-catch-finally/try-catch.js
+++ exercicios/try-catch-finally/try-catch.js
@@ -1,14 +1,34 @@
-// Sintaxe do try-catch:
-    // try {
-        // 
-    // } catch (error) {
-        // 
-    // }
 
-try {
-    throw "teste"
-    throw 21 // não executa
-    throw true // não executa
-} catch (err) {
-    console.log(err)
+function syntaxTryCatch() {
+    try {
+        // trecho de código que pode gerar erros
+    } catch (err) {
+        switch (true) {
+            case err instanceof ExceptionExemple:
+                // tratamento especifico para uma exception lançada pelo 'ExceptionExemple'
+                break
+            case err instanceof ExceptionExempleElse:
+            // tratamento especifico para uma exception lançada pelo 'ExceptionExempleElse'
+                break
+            default:
+                // tratamento genérico para ademais erros
+                break
+        }
+    } finally {
+        // trecho de código que será executado idependente se foi ocasionado um erro ou não
+    }
 }
+
+function throwExempleErrors() {
+    try {
+        throw "teste"
+        throw 21 // não executa
+        throw true // não executa
+    } catch (err) {
+        console.log(err)
+    } finally {
+        console.log('FIM DO PROGRAMA...')
+    }
+}
+
+throwExempleErrors()
